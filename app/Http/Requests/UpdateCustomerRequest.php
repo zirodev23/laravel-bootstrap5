b5ed9fbc1c9430c63fba8ee0d0c0a29fcd117b66
zirodev <zirodev23@zirodev23.com>
2023-11-19 19:06:36 +0200

    full customer crud functionality added

--- app/Http/Requests/UpdateCustomerRequest.php
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -11,7 +11,7 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateCustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:customers',
+            'phone' => 'required|unique:customers'
         ];
     }
 }
--- database/migrations/2023_11_12_165528_create_customers_table.php
+++ database/migrations/2023_11_12_165528_create_customers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
